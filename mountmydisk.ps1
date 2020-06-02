d5ac5cd9f82c0ee1f93469e8385ae7583c132a78
Habib <65003028+habib-Bashi@users.noreply.github.com>
2020-06-02 14:36:24 +1000

    Update mountmydisk.ps1

--- mountmydisk.ps1
+++ mountmydisk.ps1
@@ -7,7 +7,7 @@ $labels = "data1","data2"
 foreach($disk in $disks){
     $driverletter = $letters[$count].ToString()
     $disk |
-    Initialize-Disk -PartitionStyle MBR -PassThru()
+    Initialize-Disk -PartitionStyle MBR -PassThru |
     New-Partition -UseMaximumSize -DriveLetter $driverletter |
     Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force
     $count++
