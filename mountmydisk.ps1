361ba2874758bd1019ac5980e4038efc17ec8dd1
Habib <65003028+habib-Bashi@users.noreply.github.com>
2020-06-02 14:25:34 +1000

    Create mountmydisk.ps1
    mount by extension in powershell

---
+++ mountmydisk.ps1
@@ -0,0 +1,14 @@
+$disks = Get-Disk | Where PartitionStyle -eq 'raw' | sort Number
+
+$letters = 70..89 | ForEach-Object {[char]$_}
+$count = 0
+$labels = "data1","data2"
+
+foreach($disk in $disks){
+    $driverletter = $letters[$count].ToString()
+    $disk |
+    Initialize-Disk -PartitionStyle MBR -PassThru()
+    New-Partition -UseMaximumSize -DriveLetter $driverletter |
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force
+    $count++
+}
